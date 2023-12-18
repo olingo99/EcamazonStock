b2b943087547aaa7a7e30e23764a719c566943fa
Olingo <195367@ecam.be>
2023-12-18 15:15:50 +0100

    first test

--- EcamazonStock/settings.py
+++ EcamazonStock/settings.py
@@ -81,7 +81,12 @@ WSGI_APPLICATION = 'EcamazonStock.wsgi.application'
 host = os.getenv('POSTGRES_HOST', 'localhost')
 port = os.getenv('POSTGRES_PORT', '5432')
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
